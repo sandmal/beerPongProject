3b159e85be047ddde2ca9ea4948b4e8bd6955755
Cornelius <sandmal.cornelius@gmail.com>
2022-01-01 18:16:25 +0100

    Added profile data

--- frontend/src/components/users/Profile.tsx
+++ frontend/src/components/users/Profile.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import { gql, useQuery } from '@apollo/client';
+
+const SHOWPROFILE = gql`
+  query {
+    me {
+      name
+      email
+    }
+  }
+`;
+
 function Profile() {
+  const { loading, error, data } = useQuery(SHOWPROFILE);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
   return (
     <div>
       <p>This is the profile page</p>
+      <h3>Name: {data.me.name}</h3>
+      <p>Email: {data.me.email}</p>
     </div>
   );
 }
