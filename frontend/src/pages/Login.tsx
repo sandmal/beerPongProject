c554fafc1b206e2701d94eeec1e03b1652b69d9e
Cornelius <sandmal.cornelius@gmail.com>
2022-01-20 22:32:50 +0100

    Created login and register form css (temp)

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -18,37 +18,59 @@ export default function Login() {
 
   const { dispatch } = useContext(AuthContext);
   return (
-    <div>
-      <h1>Login</h1>
-      <Formik
-        initialValues={DefaultSignValues}
-        validationSchema={validationSchema}
-        onSubmit={async (values, { setSubmitting }) => {
-          setSubmitting(true);
-          const loginData = await login({
-            variables: { input: { email: values.email, password: values.password } },
-          });
-          let decodedToken: any = jwt(loginData.data.login);
-          if (loginData.data.login !== null) {
-            const email = decodedToken.email;
-            const isRegistered = decodedToken.active;
-            dispatch({ type: 'USER_LOGIN', payload: { email: email, isRegistered: isRegistered } });
-            navigate('/user');
-          }
-          setSubmitting(false);
-        }}>
-        <Form>
-          <Field name='email' type='text' placeholder='Email' />
-          <ErrorMessage name='email' component={'div'} />
-
-          <Field name='password' type='password' placeholder='Password' />
-          <ErrorMessage name='password' component={'div'} />
-
-          <button type='submit' className='login-button'>
-            <span>Login</span>
-          </button>
-        </Form>
-      </Formik>
+    <div className='w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0'>
+      <div className='w-full sm:max-w-md p-5 mx-auto'>
+        <h2 className='mb-12 text-center text-5xl font-extrabold'>Login</h2>
+        <Formik
+          initialValues={DefaultSignValues}
+          validationSchema={validationSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            setSubmitting(true);
+            const loginData = await login({
+              variables: { input: { email: values.email, password: values.password } },
+            });
+            let decodedToken: any = jwt(loginData.data.login);
+            if (loginData.data.login !== null) {
+              const email = decodedToken.email;
+              const isRegistered = decodedToken.active;
+              dispatch({ type: 'USER_LOGIN', payload: { email: email, isRegistered: isRegistered } });
+              navigate('/user');
+            }
+            setSubmitting(false);
+          }}>
+          <Form>
+            <div className='mb-4'>
+              <label className='block mb-1' htmlFor='email'>
+                Email-Address
+              </label>
+              <Field
+                className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+                name='email'
+                type='text'
+              />
+              <ErrorMessage name='email' component={'div'} />
+            </div>
+            <div className='mb-4'>
+              <label className='block mb-1' htmlFor='password'>
+                Password
+              </label>
+              <Field
+                className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+                name='password'
+                type='password'
+              />
+              <ErrorMessage name='password' component={'div'} />
+            </div>
+            <div className='mt-6'>
+              <button
+                type='submit'
+                className='w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition'>
+                Login
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -21,39 +21,77 @@ function Register() {
       .required('Password is required'),
   });
   return (
-    <div>
-      <h1>Register</h1>
-      <Formik
-        initialValues={DefaultRegisterValues}
-        validationSchema={validationSchema}
-        onSubmit={async (values, { setSubmitting }) => {
-          setSubmitting(true);
-          const registerData = await register({
-            variables: { input: { name: values.name, email: values.email, password: values.password } },
-          });
+    <div className='w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0'>
+      <div className='w-full sm:max-w-md p-5 mx-auto'>
+        <h2 className='mb-12 text-center text-5xl font-extrabold'>Register</h2>
+        <Formik
+          initialValues={DefaultRegisterValues}
+          validationSchema={validationSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            setSubmitting(true);
+            const registerData = await register({
+              variables: { input: { name: values.name, email: values.email, password: values.password } },
+            });
 
-          if (registerData.data.registerUser !== null) {
-            navigate('/login');
-          }
-        }}>
-        <Form>
-          <Field name='name' type='text' placeholder='Name' />
-          <ErrorMessage name='name' component={'div'} />
-
-          <Field name='email' type='text' placeholder='Email' />
-          <ErrorMessage name='email' component={'div'} />
-
-          <Field name='password' type='password' placeholder='Password' />
-          <ErrorMessage name='password' component={'div'} />
-
-          <Field name='confirmPassword' type='password' placeholder='Confirm Password' />
-          <ErrorMessage name='confirmPassword' component={'div'} />
-
-          <button type='submit' className='login-button'>
-            <span>Register</span>
-          </button>
-        </Form>
-      </Formik>
+            if (registerData.data.registerUser !== null) {
+              navigate('/login');
+            }
+          }}>
+          <Form>
+            <div className='mb-4'>
+              <label className='block mb-1' htmlFor='name'>
+                Name
+              </label>
+              <Field
+                className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+                name='name'
+                type='text'
+              />
+              <ErrorMessage name='name' component={'div'} />
+            </div>
+            <div className='mb-4'>
+              <label className='block mb-1' htmlFor='email'>
+                Email-Address
+              </label>
+              <Field
+                className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+                name='email'
+                type='text'
+              />
+              <ErrorMessage name='email' component={'div'} />
+            </div>
+            <div className='mb-4'>
+              <label className='block mb-1' htmlFor='password'>
+                Password
+              </label>
+              <Field
+                className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+                name='password'
+                type='password'
+              />
+              <ErrorMessage name='password' component={'div'} />
+            </div>
+            <div className='mb-4'>
+              <label className='block mb-1' htmlFor='confirmPassword'>
+                Confirm Password
+              </label>
+              <Field
+                className='py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full'
+                name='confirmPassword'
+                type='password'
+              />
+              <ErrorMessage name='confirmPassword' component={'div'} />
+            </div>
+            <div className='mt-6'>
+              <button
+                type='submit'
+                className='w-full inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition'>
+                Register
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 }
