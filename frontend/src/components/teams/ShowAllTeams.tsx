2be72f7096ce6e07201be13698fd22eeb0766466
Cornelius <sandmal.cornelius@gmail.com>
2022-01-23 23:14:09 +0100

    Added styling for teams and a little for discover

--- frontend/src/components/teams/ShowAllTeams.tsx
+++ frontend/src/components/teams/ShowAllTeams.tsx
@@ -9,17 +9,27 @@ function ShowAllTeams() {
   return (
     <div>
       <p>This is where you find all teams</p>
-      <>
+      <div className='flex justify-around'>
         {data.teams.map((team: any) => (
-          <div key={team.teamId}>
+          <div key={team.teamId} className=' py-5'>
             <h3>Name: {team.name}</h3>
             <p>Created by: {team.creator}</p>
             <p>Description: {team.description}</p>
             <p>Size: {team.size}</p>
-            {team.members.length === Number(team.size) ? <p>full</p> : <button>Join team</button>}
+            {team.members.length === Number(team.size) ? (
+              <button
+                disabled
+                className='w-full inline-flex items-center justify-center px-4 py-1 bg-red-500 border border-transparent rounded-md font-semibold text-white focus:outline-none transition'>
+                Team full
+              </button>
+            ) : (
+              <button className='w-full inline-flex items-center justify-center px-4 py-1 bg-blue-500 border border-transparent rounded-md font-semibold text-white hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring-blue-200 disabled:opacity-25 transition'>
+                Join team
+              </button>
+            )}
           </div>
         ))}
-      </>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Discover.tsx
+++ frontend/src/pages/Discover.tsx
@@ -2,7 +2,7 @@ import ShowAllTeams from '../components/teams/ShowAllTeams';
 
 function Discover() {
   return (
-    <div>
+    <div className='px-16 py-6'>
       <h1>This is the teams dashboard</h1>
       <p>See all teams statistics, team information and more.</p>
       <p>Join teams</p>
